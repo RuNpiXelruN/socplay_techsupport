101cfb061c474535e30a6d72bebc036544340cac
runpixelrun <justindavidson23@gmail.com>
2015-08-18 12:46:00 +0000

    Added posts model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   
   devise_for :users
   
+  resources :posts
   
-  
-  root 'pages#index'
+  root 'posts#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150816120553) do
+ActiveRecord::Schema.define(version: 20150818123721) do
 
   create_table "plans", force: :cascade do |t|
     t.string   "name"
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
